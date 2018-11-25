bb2cb409a330263932d2f748647f08a7ba0d74bf
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-25 12:41:47 -0800

    (refactor) Remove commented out code

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,6 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     return (
       <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <ProfileIcon onRouteChange={onRouteChange} />
-        {/* <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p> */}
       </nav>
     );
   } else {
