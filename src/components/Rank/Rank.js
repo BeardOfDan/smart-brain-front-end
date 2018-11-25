8b737833f72f6bfaa386070c106074610c10c125
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-25 12:25:02 -0800

    (feat) Start to add emoji badge

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      emoji: ''
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    const emojis = ['🤔', '😀', '😁', '😊', '🔶', '🔷', '🚀'];
+
+    const emoji = (entries > (emojis.length - 1)) ? emojis[emojis.length - 1] : emojis[entries];
+
+    this.setState({ emoji });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='white f3'>
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {this.props.entries}
+        </div>
+        {/* <div className='white f3'>
+          {`Rank Badge: ${this.state.emoji}`}
+        </div> */}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
